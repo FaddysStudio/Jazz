5ec2c8b0117925567767c1432b9c12e00ad523c2
Faddy Michel Samaan <faddy.michel@gmail.com>
2024-05-20 07:25:51 -0400

    @faddymichel: On branch main
    *	modified:   .gitignore
    *	new file:   beat.rs

---
+++ beat.rs
@@ -0,0 +1,75 @@
+# Faddy's Beat
+
+?# mkdir -p .FaddysBeat ; ln -fs ~/FaddysStudio/kit/percussive/*.wav .FaddysBeat
+
+?# -1 -2 cd .FaddysBeat ; npm i @faddys/scenarist
+
+?# cat - > .FaddysBeat/index.mjs
+
++==
+import Scenarist from '@faddys/scenarist';
+
+await Scenarist ( new class Beat {
+
+track = "track-" + Date .now ()
+
+$track ( $, value, ... argv ) {
+
+this .track = value;
+
+return $ ( ... argv );
+
+}
+
+instrument = 13
+tempo = 110;
+
+$tempo ( $, value, ... argv ) {
+
+this .tempo = parseFloat ( value ) || this .tempo;
+
+return $ ( ... argv );
+
+}
+
+length = 4
+measure = 8
+sequence = []
+
+async $_producer ( $ ) {
+
+await $ ( ... process .argv .slice ( 2 ) );
+
+const beat = this;
+
+console .log ( `
+
+t 0 ${ beat .tempo }
+v ${ beat .length }
+
+${ beat .sequence .join ( '\n' ) }
+
+s ${ beat .length }
+
+` .trim () );
+
+}
+
+async $_director ( $, ... argv ) {
+
+if ( ! argv .length )
+return '';
+
+const beat = this;
+const [ step, sample ] = argv .shift () .split ( '/' );
+
+beat .sequence .push ( `i ${ beat .instrument }.${ beat .sequence .length } [${ step }/${ beat .measure }] 1 "${ beat .track }" ".FaddysBeat/${ sample }.wav"` );
+
+await $ ( ... argv );
+
+}
+
+} );
+-==
+
+?# $ node .FaddysBeat/index.mjs
